Added weight gradient plot; Added layer widget; Fixed activation functions in tests; Clearing distribution img when no data; Using QAction instead of QToolButton; Removed parent param from BasePlot and GradientLengthPlot; Fixed DistributionPlot property name;

ui/plot/distribution.py
@@ -18,21 +18,24 @@ class DistributionPlot(BasePlot):
 
         self._bar = pg.ColorBarItem(values=(0, 10))
         self._img = pg.ImageItem()
-        self.image_transform = QTransform()
-        self.image_transform.scale(1/self._resolution, 1/self._resolution)
-        self._img.setTransform(self.image_transform)
+        self._image_transform = QTransform()
+        self._image_transform.scale(1 / self._resolution, 1 / self._resolution)
+        self._img.setTransform(self._image_transform)
         self._item.addItem(self._img)
 
     def set_data(self, metrics: list[ai.TrainMetric]):
-        neurons = []
-        outputs = []
-        for metric in metrics:
-            for output in metric.outputs:
-                for neuron, activation in enumerate(output):
-                    neurons.append(neuron)
-                    outputs.append(activation)
-        bin_x = self._resolution
-        bin_y = np.linspace(0, 1, self._resolution)
-        hist, _, _ = np.histogram2d(neurons, outputs, bins=(bin_x, bin_y), normed=False)
-        self._img.setImage(image=hist)
+        if metrics:
+            neurons = []
+            outputs = []
+            for metric in metrics:
+                for output in metric.outputs:
+                    for neuron, activation in enumerate(output):
+                        neurons.append(neuron)
+                        outputs.append(activation)
+            bin_x = self._resolution
+            bin_y = np.linspace(0, 1, self._resolution)
+            hist, _, _ = np.histogram2d(neurons, outputs, bins=(bin_x, bin_y), normed=False)
+            self._img.setImage(image=hist)
+        else:
+            self._img.clear()
         self._bar.setImageItem(self._img, insert_in=self._item)