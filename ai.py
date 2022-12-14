Added advanced gradient & state widget; Removed w and b gradient widgets; Removed global layer widget; Fixed layer count evaluation; Added util for finding enum by its value; Act funcs in are tuple, not list; Optimized imports;

ai.py
@@ -152,7 +152,7 @@ class Ai:
         validate_brain(weights, biases)
         self.w: list[np.ndarray] = weights
         self.b: list[np.ndarray] = biases
-        self.activation_functions: list[ActivationFunction] = activation_functions
+        self.activation_functions: tuple[ActivationFunction] = activation_functions
         self.learning_rate = learning_rate
         self.data_used = 0
 